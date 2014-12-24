8a840a217c37b5ec4cd73c364a883a2cc18a4266
chrischris292 <cbchan2@illinois.edu>
2014-12-23 19:14:11 -0800

    connected to MongoDB server and gotkeys()

--- index.js
+++ index.js
@@ -5,21 +5,63 @@ var server = require('http').createServer(app);
 var port = process.env.PORT || 3000;
 var csv = require('fast-csv');
 var fs = require('fs');
-
-
+var MongoClient = require('mongodb').MongoClient;
+// Routing
+app.use(express.static(__dirname + '/public'));
 server.listen(port, function () {
   console.log('Server listening at port %d', port);
 });
+var pbpData = []
+var keyArray
+var i = 0;
+getKeys();
 
-// Routing
-app.use(express.static(__dirname + '/public'));
+function querySeahawksPlays(db){
+  var penaltyData = [];
+  for(key in keyArray)
+  {
+      var collection = db.collection(keyArray[key]);
+      console.log(key + "  " + keyArray[key])
+      collection.find({"IsPenalty" : "1" ,"PenaltyTeam" : "SEA"}).toArray(function(err,docs){
+      console.log(docs[0].DefenseTeam + " " + docs.length)
 
-var stream = fs.createReadStream(__dirname + '/data/pbp-2014.csv');
+        penaltyData.push(docs);
+        if(key==keyArray.length-1){
+            app.get('/data', function(req, res){
+            var asJSON = JSON.stringify(penaltyData);
+            res.send(asJSON)
+        })
 
+        }
+      })
+  }
+
+}
+//helper functions
+function connecToMongo(){
+  // Connect to the db
+  MongoClient.connect("mongodb://localhost:27017/Seahawks", function(err, db) {
+    if(err) throw err;
+    for(key in pbpData){
+          var collection = db.collection(key);
+          collection.insert(pbpData[key], function(err, docs) {
+          });
+    }
+  });
+  db.close();
+
+}
 // Chatroom
 //my own stuff
-var pbpData = []
-var i = 0;
+//Temp communication to front end for testing. 
+
+//Data structure for NFL data
+function nflData(){
+  this.gameID
+}
+function readCSV(){
+var stream = fs.createReadStream(__dirname + '/data/pbp-2014.csv');
+
 
 var csvStream = csv.fromStream(stream,{headers:true})
     .on("data", function(data){
@@ -36,16 +78,29 @@ var csvStream = csv.fromStream(stream,{headers:true})
       }
     })
     .on("end", function(){
+          console.log(pbpData[2014121412])
+     connecToMongo();
     });
+}
 
-//Temp communication to front end for testing. 
-app.get('/data', function(req, res){
-    var asJSON = JSON.stringify(pbpData);
-     res.send(pbpData)
-})
-
-//Data structure for NFL data
-function nflData(){
-  this.gameID
-
+function getKeys(){
+  var badKey;
+    MongoClient.connect("mongodb://localhost:27017/Seahawks", function(err, db) {
+    db.collectionNames(function(err, items) {
+      for(key in items){
+          var tempName = items[key].name;
+          if(tempName!="Seahawks.system.indexes")
+          {
+            items[key]=tempName.split(".")[1];
+          }
+          else{
+            badKey = key;
+          }
+      }
+      items.splice(badKey,1)
+      keyArray = items.slice(0);
+      querySeahawksPlays(db)
+      });
+        
+    })
 }
